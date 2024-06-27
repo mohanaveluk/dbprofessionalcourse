c6fa30ba7fb98915b29a18fc9509a79886fc8181
mohanaveluk <mohanaveluk>
2024-06-27 02:52:31 +0000

    add

---
+++ formula1/set-ups/01-access-adls-using-access-keys.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# spark.conf.set(
+#     "fs.azure.account.key.invoicesystem.dfs.core.windows.net", #"G3oEPO2OyzIxtBkbEEE9ODf7I91nBveRRwXv2EVmJdpywVZ9pOU+SulPoTzizrYV5bKnQchrFkR+AStWAh2xQ==")
